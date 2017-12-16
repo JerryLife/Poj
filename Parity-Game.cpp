a4a5d03fb076bc58e8761076c840c73c6278bf8d
Jerry_Life <HUSTWuzhaomin@outlook.com>
2017-12-16 12:12:20 +0800

    Food-Chain Add

--- Parity-Game.cpp
+++ Parity-Game.cpp
@@ -2,17 +2,22 @@
 #include <cstdio>
 #include <algorithm>
 #include <cstring>
-#include <cmath>
-#include <queue>
-#include <set>
-#include <vector>
 
 #define maxn 11111
 
 using namespace std;
+struct Claim {
+    int u;
+    int v;
+    int w;
+};
+
 
 int r[maxn], p[maxn], f[maxn];
+Claim cs[maxn];
 
+// UnionFind from right to left
+// r indicates relation (0 for even, 1 for odd)
 int Find(int x) {
     if (x == p[x])
         return x;
@@ -21,10 +26,56 @@ int Find(int x) {
     return p[x];
 }
 
+int Union(int a, int b, int w) {
+    int pa = Find(a);
+    int pb = Find(b);
+    if (pa == pb) {
+        return (r[a] ^ r[b] == w);
+    }
+    else {
+        // pa > pb
+        r[pa] = r[a] ^ r[b] ^ w;
+        p[pa] = pb;
+        return 1;
+    }
+}
+
 int main() {
     int n, m;
     scanf("%d%d", &n, &m);
-    while (m-- > 0) {
 
+    // read all data, and create a hash map
+    int cnt = 0;
+    for (int i = 0; i < m; ++i) {
+        Claim c;
+        char s[10];
+        scanf("%d%d%s", &c.u, &c.v, s);
+        c.u --;         // this line is really important
+        c.w = strcmp(s, "even");  // even = 0, odd = 1
+        cs[i] = c;
+        f[cnt++] = c.u;
+        f[cnt++] = c.v;
     }
+
+    sort(f, f + cnt);       // sort the hash table
+    cnt = (int)(unique(f, f + cnt) - f);    // remove duplicate
+
+    // initialize
+    for (int i = 0; i < cnt; ++i) {
+        p[i] = i;
+        r[i] = 0;
+    }
+
+    int i;
+    for (i = 0; i < m; ++i) {
+        // find elements in hash table
+        int a = (int)(lower_bound(f, f + cnt, cs[i].u) - f);
+        int b = (int)(lower_bound(f, f + cnt, cs[i].v) - f);
+
+        // check if collision
+        if (Union(a, b, cs[i].w) == 0)
+            break;
+    }
+    printf("%d\n", i);
+    return 0;
 }
