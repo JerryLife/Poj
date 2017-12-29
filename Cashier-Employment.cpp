3cc10b8833b9756c5acb151100ab58e3c12e2f4d
Jerry_Life <HUSTWuzhaomin@outlook.com>
2017-12-29 14:45:47 +0800

    Cashier Employment finished.

--- Cashier-Employment.cpp
+++ Cashier-Employment.cpp
@@ -20,7 +20,8 @@ struct Edge {
     Edge(int v, int w, int next) : v(v), w(w), next(next) {}
 } edges[mmax];
 
-int n, m, cnt = 0;
+int n, m, cnt = 0, clerk_n;
+int r[25], t[25];
 int head[nmax], d[nmax];
 int visited[nmax], v_time[nmax];
 
@@ -30,18 +31,41 @@ int addEdge(int u, int v, int w) {
 }
 
 int init() {
-    memset(head, -1, sizeof(head));
-    memset(visited, 0, sizeof(visited));
-    memset(v_time, 0, sizeof(v_time));
-    memset(d, 0x3f, sizeof(d));
+    memset(r, 0, sizeof(r));
+    memset(t, 0, sizeof(t));
 }
 
-bool SPFA() {
-    // start at 1
-    d[1] = 0;
+int clear() {
+    memset(visited, 0, sizeof(visited));
+    memset(v_time, 0, sizeof(v_time));
+    memset(d, -1, sizeof(d));
+}
+
+int build(int sum) {
+    cnt = 0;
+    memset(head, -1, sizeof(head));
+    addEdge(0, 24, sum);
+    for (int i = 1; i <= 24; ++i) {
+        addEdge(i - 1, i, 0);
+        addEdge(i, i - 1, -t[i]);
+    }
+    for (int i = 1; i <= 24; ++i) {
+        if (i <= 8)
+            addEdge(i + 16, i, r[i] - sum);
+        else
+            addEdge(i - 8, i, r[i]);
+    }
+}
+
+bool SPFA(int sum) {
+    clear();
+    // start at 0
+    int start = 0;
+    d[start] = 0;
+    visited[start] = true;
     // pair< distance, node > ranked as distance
     priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > q;
-    q.push(pair<int, int>(0, 1));
+    q.push(pair<int, int>(0, start));
 
     while (!q.empty()) {
         pair<int, int> p;   // top element
@@ -49,14 +73,14 @@ bool SPFA() {
         q.pop();
         visited[p.second] = false;
 
-        if (v_time[p.second]++ > n)
+        if (v_time[p.second]++ > 24)
             return false;   // negative loop
 
         for (int i = head[p.second]; i != -1; i = edges[i].next) {
             int u = p.second;
             int v = edges[i].v;
             int w = edges[i].w;
-            if (d[u] + w < d[v]) {
+            if (d[u] + w > d[v]) {
                 d[v] = d[u] + w;
                 if (!visited[v]) {
                     visited[v] = true;
@@ -66,18 +90,40 @@ bool SPFA() {
         }
     }
 
-    return true;
+    return (d[24] == sum);
+}
+
+int scan() {
+    for (int i = 0; i <= clerk_n; ++i) {
+        build(i);
+        if (SPFA(i)) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main() {
-    scanf("%d %d", &n, &m);
-    init();
-    for (int i = 0; i < m; ++i) {
-        int u, v, w;
-        scanf("%d%d%d", &u, &v, &w);
-        addEdge(u, v, w);
+
+    scanf("%d", &n);
+    while (n--) {
+        init();
+        // deal with input
+        for (int i = 1; i <= 24; ++i) {
+            scanf("%d", r + i);
+        }
+        scanf("%d", &clerk_n);
+        for (int i = 1; i <= clerk_n; ++i) {
+            int tmp;
+            scanf("%d", &tmp);
+            ++t[tmp + 1];
+        }
+
+        int rst;
+        if ((rst = scan()) != -1)
+            printf("%d\n", rst);
+        else
+            printf("No Solution\n");
     }
-    if (SPFA())
-        printf("%d", d[n]);
     return 0;
 }
