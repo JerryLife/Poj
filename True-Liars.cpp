21961bf3c0dc4c00eb16ec40161f29afeb645f4b
Jerry_Life <HUSTWuzhaomin@outlook.com>
2018-01-08 17:23:22 +0800

    True-liars finished.

--- True-Liars.cpp
+++ True-Liars.cpp
@@ -1,67 +1,119 @@
-//
-// Part 1.3: Poj 1417
-
-#include <iostream>
 #include <cstdio>
-#include <algorithm>
-#include <cstring>
+#include<cstring>
+#include<map>
+
+#define nmax 650
 
 using namespace std;
 
-const int maxn = 650;
+typedef map<int, int> Map;
 
-int rel[maxn], pre[maxn];
+int bag[nmax][2];
+Map bagmap;
+int cnt;
+int dp[nmax][310];
+int pa[nmax];
+int re[nmax];
+int choose[nmax];
 
-int Find(int x) {
-    int px = pre[x];
-    if (px == x)
-        return x;
-    pre[x] = Find(px);
-    rel[x] = rel[x] ^ rel[px];
-    return pre[x];
+int insert(int x,int b) {
+    if (bagmap.find(x) == bagmap.end())
+        bagmap[x] = ++cnt;
+    ++bag[bagmap[x]][b];//该分量的b类人加1
+    return bagmap[x];
 }
 
-void Union(int a, int b, int w) {
-    int pa = Find(a);
-    int pb = Find(b);
-    if (pa != pb) {
-        pre[pa] = pb;
-        rel[pa] = rel[a] ^ rel[b] ^ w;
+int Find(int i) {
+    if (pa[i] == -1)
+        return i;
+    int tmp = Find(pa[i]);
+    re[i] = (re[i] + re[pa[i]]) & 1;
+    return pa[i] = tmp;
+}
+
+void Union(int u,int v,int w) {
+    int fa = Find(u);
+    int fb = Find(v);
+    if (fa != fb) {
+        pa[fb] = fa;
+        re[fb] = (re[u] + re[v] + w) & 1;
     }
 }
 
 void Init() {
-    for (int i = 0; i < maxn; ++i) {
-        pre[i] = i;
-    }
-    memset(rel, 0, maxn);
-
+    cnt = 0;
+    bagmap.clear();
+    memset(bag, 0, sizeof(bag));
+    memset(pa, -1, sizeof(pa));
+    memset(re, 0, sizeof(re));
+    memset(dp, 0, sizeof(dp));
+    memset(choose, -1, sizeof(choose));
 }
 
 int main() {
-
     int n, p1, p2;
-
-    while (true) {
-        scanf("%d%d%d", &n, &p1, &p2);
-        if (!n && !p1 && !p2)
-            break;
-
+    while (scanf("%d%d%d", &n, &p1, &p2)) {
+        if (!(n || p1 || p2))
+            return 0;
         Init();
-        // Union all the data
-        while (n--) {
-            int u, v, w;
-            char s[5];
-            scanf("%d%d%s", &u, &v, s);
-            w = (s[0] == 'n');
-            Union(u, v, w);
+        for (int l = 0; l < n; ++l) {
+            int a, b, type = 0;
+            char str[15];
+            scanf("%d%d%s", &a, &b, str);
+            switch (str[0]) {
+                case 'y': type = 0;
+                    break;
+                case 'n': type = 1;
+                    break;
+                default:
+                    break;
+            }
+            if (Find(a) != Find(b))
+                Union(a, b, type);
+        }
+        int i = 0;
+        while (++i <= p1 + p2)
+        {
+            int fi = Find(i);
+            insert(fi, re[i]);
         }
 
-        // ensure every node's parent is root
-        for (int i = 0; i < p1 + p2; ++i) {
-            Find(i);
+        // dp
+        dp[0][0] = 1, i = 0;
+        while (++i <= cnt)
+        {
+            for (int j = 0; j <= p1; j++) {
+                for (int k = 0; k < 2; ++k) {
+                    if (j >= bag[i][k])
+                        dp[i][j] = k * dp[i][j] + dp[i - 1][j - bag[i][k]];
+                }
+            }
         }
 
+        if (dp[cnt][p1] == 1)
+        {
+            int j = p1;
+            for (int k = cnt; k >= 1; k--)
+            {
+                for (int l = 0; l < 2; ++l) {
+                    if (dp[k][j] == dp[k - 1][j - bag[k][l]]) {
+                        choose[k] = l;
+                        j -= bag[k][l];
+                        break;
+                    }
+                }
+            }
+            i = 0;
+            while (++i <= p1 + p2) {
+                int num = bagmap[Find(i)];
+                if (re[i] == choose[num])
+                    printf("%d\n", i);
+            }
+            printf("end\n");
+        } else {
+            printf("no\n");
+        }
 
     }
+    return 0;
 }
